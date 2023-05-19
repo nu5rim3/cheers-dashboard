159a5e6bafda83f6fffe4bebd237ef69656f64b0
Nusri Samath <nusrisamath@gmail.com>
2023-05-19 11:01:04 +0530

    [Updated] - Added book fliper to review the menu

--- src/pages/Basic/components/ImagePreviewer.tsx
+++ src/pages/Basic/components/ImagePreviewer.tsx
@@ -1,8 +1,146 @@
+import { Box } from '@mui/material';
 import React from 'react'
+import HTMLFlipBook from 'react-pageflip';
 
 const ImagePreviewer = () => {
+
   return (
-    <div>Image Previewer</div>
+    <Box>
+      <HTMLFlipBook
+        style={{}}
+        startPage={0}
+        width={500}
+        height={700}
+        drawShadow={true}
+        flippingTime={10}
+        usePortrait={false}
+        startZIndex={0}
+        autoSize={true}
+        clickEventForward={true}
+        useMouseEvents={true}
+        swipeDistance={0}
+        showPageCorners={true}
+        disableFlipByClick={false}
+        size="stretch"
+        minWidth={315}
+        maxWidth={1000}
+        minHeight={700}
+        maxHeight={1533}
+        maxShadowOpacity={0.5}
+        showCover={false}
+        mobileScrollSupport={true}
+        onFlip={() => { }}
+        onChangeOrientation={() => { }}
+        onChangeState={() => { }}
+        className={''}
+      >
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              // objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              // objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+        <Box>
+          <img
+            src="http://via.placeholder.com/500x700"
+            alt="cover"
+            style={{
+              float: 'left',
+              width: '100%',
+              // height: 700,
+              objectFit: 'cover'
+            }}
+            loading="lazy"
+          />
+        </Box>
+
+      </HTMLFlipBook>
+    </Box>
   )
 }
 
